e3e5243664e292bc4d37e52c0e0d67fa7e8b116a
felipecarrillo100 <felipecarrillo100@gmail.com>
2025-04-05 00:56:09 +0200

    Fixes Toolbox for angular

---
+++ toolbox/angular-pano-m1/src/app/components/luciad-map/luciad-map.component.ts
@@ -0,0 +1,125 @@
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {WebGLMap} from '@luciad/ria/view/WebGLMap.js';
+import {WMSTileSetModel} from '@luciad/ria/model/tileset/WMSTileSetModel.js';
+import {WMSTileSetLayer} from '@luciad/ria/view/tileset/WMSTileSetLayer.js';
+import {OGC3DTilesModel} from '@luciad/ria/model/tileset/OGC3DTilesModel.js';
+import {TileSet3DLayer} from '@luciad/ria/view/tileset/TileSet3DLayer.js';
+import {UrlStore} from '@luciad/ria/model/store/UrlStore.js';
+import {FeatureModel} from '@luciad/ria/model/feature/FeatureModel.js';
+import {FeatureLayer} from '@luciad/ria/view/feature/FeatureLayer.js';
+import {PanoramaFeaturePainter} from '../../../modules/luciad/painters/PanoramaFeaturePainter';
+import {CreatePanoramaControllers} from '../../../modules/luciad/pano/controller/CreatePanoramaControllers';
+import {FusionPanoramaModel} from '@luciad/ria/model/tileset/FusionPanoramaModel.js';
+import {PanoramaActions} from '../../../modules/luciad/pano/actions/PanoramaActions';
+
+@Component({
+  selector: 'app-luciad-map',
+  imports: [],
+  templateUrl: './luciad-map.component.html',
+  styleUrl: './luciad-map.component.scss'
+})
+export class LuciadMapComponent implements OnInit, OnDestroy {
+  // IMPORTANT: The div component must be set to static:true
+  @ViewChild('luciadMapContainer', { static: true }) mapContainer: ElementRef | null = null;
+
+  private map: WebGLMap | null = null;
+
+  // Initialize map
+  ngOnInit(): void {
+    if (this.mapContainer && this.mapContainer.nativeElement !== null) {
+      this.map = new WebGLMap(this.mapContainer.nativeElement, {reference: "epsg:4978"});
+      LoadLayers(this.map);
+
+    } else {
+      console.error(`Can't find the ElementRef reference for mapContainer)`);
+    }
+  }
+
+  // Destroy map
+  ngOnDestroy(): void {
+    this.map?.destroy();
+  }
+
+  editbbox() {
+    alert("ABC")
+  }
+}
+
+
+function LoadLayers(map: WebGLMap) {
+  const wmsUrl = "https://sampleservices.luciad.com/wms";
+
+  const layerImageryName = [{layer: "4ceea49c-3e7c-4e2d-973d-c608fb2fb07e"}];
+
+  // Adds a WMS layer as a background
+  WMSTileSetModel.createFromURL(wmsUrl, layerImageryName, {}).then(async (model: WMSTileSetModel) => {
+    const layer = new WMSTileSetLayer(model, {
+      label: "Satellite Imagery",
+    });
+    map.layerTree.addChild(layer);
+
+    // Once whe WMS layer has been loaded the Mesh layer
+    addMeshLayer(map).then(()=>{
+      addPanoramaLayer(map);
+    });
+  });
+}
+
+
+
+// Adding a Memory Store
+function addMeshLayer(map: WebGLMap) {
+  return new Promise<TileSet3DLayer>((resolve)=>{
+    const url = "https://sampledata.luciad.com/data/ogc3dtiles/LucerneAirborneMesh/tileset.json"
+    OGC3DTilesModel.create(url, {}).then((model:OGC3DTilesModel)=>{
+      //Create a layer for the model
+      const layer = new TileSet3DLayer(model, {
+        label: "Mesh Layer",
+      });
+
+      //Add the layer to the map
+      map.layerTree.addChild(layer);
+      resolve(layer);
+    });
+  });
+}
+
+function addPanoramaLayer(map: WebGLMap) {
+  const target = "https://sampledata.luciad.com/data/panoramics/LucernePegasus/cubemap_final.json";
+  const store = new UrlStore({
+    target
+  });
+  const model = new FeatureModel(store);
+
+  const panoModel = new FusionPanoramaModel(target);
+
+  const layer = new FeatureLayer(model, {
+    panoramaModel: panoModel,
+    selectable: false,
+    hoverable: true,
+    painter:new PanoramaFeaturePainter({
+      overview: false,
+      iconHeightOffset: 0
+    })
+  });
+
+  //Add the layer to the map
+  map.layerTree.addChild(layer);
+
+  //fit on the Panorama layer
+  const queryFinishedHandle = layer.workingSet.on("QueryFinished", () => {
+    if (layer.bounds) {
+      //#snippet layerFit
+      map.mapNavigator.fit({
+        bounds: layer.bounds,
+        animate: true
+      });
+      //#endsnippet layerFit
+    }
+    queryFinishedHandle.remove();
+  });
+
+  const panoramaActions = new PanoramaActions(map as WebGLMap)
+  map.controller = CreatePanoramaControllers(panoramaActions, map, layer);
+
+}
