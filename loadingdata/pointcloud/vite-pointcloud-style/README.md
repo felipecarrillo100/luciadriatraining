e59acec5186f43518d87c0a3a8c02ea9ffc2f2a1
felipecarrillo100 <felipecarrillo100@gmail.com>
2025-04-16 21:52:58 +0200

    Style point cloud data on world height

---
+++ loadingdata/pointcloud/vite-pointcloud-style/README.md
@@ -0,0 +1,15 @@
+## Styling Point Cloud Data
+
+The `TileSet3DLayer` in LuciadRIA offers an extensive array of styling options, empowering you to customize the visual representation of your 3D data to fulfill specific aesthetic and functional needs. By tailoring these styles, you can significantly enhance the clarity, emphasis, and overall impact of your visualizations.
+
+To apply a custom style to your point cloud data, you can set the `pointCloudStyle` property of the `TileSet3DLayer`:
+
+```typescript
+layer.pointCloudStyle = YOUR_CUSTOM_STYLE;
+```
+
+For comprehensive details on defining and applying these styles, please refer to the [PointCloudStyle interface documentation](https://dev.luciad.com/portal/productDocumentation/LuciadRIA/docs/reference/LuciadRIA/interfaces/_luciad_ria_view_style_PointCloudStyle.PointCloudStyle.html). Additionally, you can explore the [guide on styling and filtering point clouds](https://dev.luciad.com/portal/productDocumentation/LuciadRIA/docs/articles/howto/ogc3dtiles/styling_filtering_point_clouds.html?subcategory=ria_hspc) for more practical insights.
+
+These resources provide detailed guidance on various visual parameters you can adjust, such as color, transparency, and other visual effects.
+
+By leveraging these styling capabilities, LuciadRIA enables developers to create immersive and interactive 3D visualizations. These visualizations are ideal for a broad spectrum of applications, ranging from detailed urban planning models to dynamic virtual simulations, enhancing both the user experience and the utility of the data presented.
---
+++ loadingdata/pointcloud/vite-pointcloud-style/src/components/LuciadMap/LuciadMap.tsx
@@ -0,0 +1,144 @@
+import React, {useEffect, useRef} from "react";
+import {WebGLMap} from "@luciad/ria/view/WebGLMap.js";
+import {getReference} from "@luciad/ria/reference/ReferenceProvider.js";
+import {WMSTileSetLayer} from "@luciad/ria/view/tileset/WMSTileSetLayer.js";
+import {WMSTileSetModel} from "@luciad/ria/model/tileset/WMSTileSetModel.js";
+import {TileSet3DLayer} from "@luciad/ria/view/tileset/TileSet3DLayer.js";
+import {HSPCTilesModel} from "@luciad/ria/model/tileset/HSPCTilesModel.js";
+import "./LuciadMap.css";
+import {
+    color,
+    distance,
+    fraction,
+    mixmap,
+    numberParameter,
+    pointParameter,
+    positionAttribute
+} from "@luciad/ria/util/expression/ExpressionFactory.js";
+
+const COLOR_SPAN_HEIGHT_V2 = [
+    "rgb( 51,102,  0)",
+    "rgb( 51,102,  0)",
+    "rgba( 60,179,113, 0.9)",
+    "rgb(187,228,146)",
+    "rgba( 222,184,135, 0.9)",
+    "rgba( 205,102,  0, 0.9)",
+    "rgba( 139, 69, 19, 0.9)",
+    "rgba( 238,213,183, 0.9)",
+    "rgba( 238,238,224, 0.9)",
+    "rgba( 255,250,250, 0.9)"
+]
+
+const TargetHSPCLayerID = "TARGET-HSPC-LAYER";
+
+export const LuciadMap: React.FC = () => {
+    const divElement = useRef(null as null | HTMLDivElement);
+    const nativeMap = useRef(null as null | WebGLMap);
+
+    useEffect(()=>{
+        // Initialize Map
+        if (divElement.current!==null) {
+            nativeMap.current = new WebGLMap(divElement.current, {reference: getReference("EPSG:4978")});
+            LoadLayers(nativeMap.current);
+        }
+        return ()=>{
+            // Destroy map
+            if (nativeMap.current) nativeMap.current.destroy();
+        }
+    },[]);
+
+    const toggleStyle = () => {
+        if (nativeMap.current) {
+            const hspcLayer = nativeMap.current?.layerTree.findLayerById(TargetHSPCLayerID);
+            if (hspcLayer instanceof TileSet3DLayer) {
+                if (hspcLayer.pointCloudStyle.colorExpression === null) {
+                    hspcLayer.pointCloudStyle.colorExpression = createPointStyle().colorExpression;
+                } else {
+                    hspcLayer.pointCloudStyle.colorExpression = null;
+                }
+            }
+        }
+    }
+
+
+    return (<div className="LuciadMap" >
+        <div ref={divElement} className="map"/>
+        <div className="button-bar">
+            <button onClick={toggleStyle}>Toggle style</button>
+        </div>
+    </div>)
+}
+
+
+
+function LoadLayers(map: WebGLMap) {
+    const wmsUrl = "https://sampleservices.luciad.com/wms";
+
+    const layerImageryName = [{layer: "4ceea49c-3e7c-4e2d-973d-c608fb2fb07e"}];
+
+    // Adds a WMS layer as a background
+    WMSTileSetModel.createFromURL(wmsUrl, layerImageryName, {}).then((model: WMSTileSetModel) => {
+        const layer = new WMSTileSetLayer(model, {
+            label: "Satellite Imagery",
+        });
+        map.layerTree.addChild(layer);
+
+        // Once whe WMS layer has been loaded the Mesh layer
+        addMeshLayer(map);
+    });
+}
+
+
+
+// Adding a Memory Store
+function addMeshLayer(map: WebGLMap) {
+    const url = "https://datamonster.myvr.net/mMap/data/pointcloud/APR/SanFrancisco/tree.hspc"
+
+    // Create the model
+    HSPCTilesModel.create(url, {}).then((model:HSPCTilesModel)=>{
+        //Create a layer for the model
+        const layer = new TileSet3DLayer(model, {
+            label: "HSPC Layer",
+            id: TargetHSPCLayerID
+        });
+
+        layer.pointCloudStyle = createPointStyle();
+
+        //Add the model to the map
+        map.layerTree.addChild(layer);
+
+        // Zoom to the point cloud location
+        map.mapNavigator.fit({ bounds: layer.bounds, animate: true });
+        console.log("Z: "+ layer.bounds.z);
+        console.log("Z+depth: "+layer.bounds.z+layer.bounds.depth);
+        console.log("Depth: " + layer.bounds.depth);
+    });
+}
+
+function createPointStyle() {
+   // const earthRadiusAtSanFransisco = 6_370_268;
+    const earthRadiusAtSanFransisco = 6370268;
+    const minParameter = numberParameter(earthRadiusAtSanFransisco - 200);
+    const maxParameter = numberParameter(earthRadiusAtSanFransisco + 300);
+
+    console.log("Min: " + minParameter.value);
+    console.log("Max: " + maxParameter.value);
+
+    const position = positionAttribute();
+    const earthCenter = pointParameter({x: 0, y: 0, z: 0});
+    const distanceToCenter = distance(position, earthCenter);
+    const heightFraction = fraction(distanceToCenter, minParameter, maxParameter);
+
+    const colorMix = COLOR_SPAN_HEIGHT_V2.map(c => {
+        return color(c);
+    });
+
+    return {
+        colorExpression: mixmap(heightFraction, colorMix)
+    }
+
+}
+
+
+
+
