08fd633cc587e9e1a66110e1c63c3824c1b99b73
felipecarrillo100 <felipecarrillo100@gmail.com>
2025-04-17 16:29:13 +0200

    Style point cloud data on world height

--- loadingdata/pointcloud/vite-pointcloud-style/src/components/LuciadMap/LuciadMap.tsx
+++ loadingdata/pointcloud/vite-pointcloud-style/src/components/LuciadMap/LuciadMap.tsx
@@ -1,17 +1,16 @@
-import React, {useEffect, useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {WebGLMap} from "@luciad/ria/view/WebGLMap.js";
 import {getReference} from "@luciad/ria/reference/ReferenceProvider.js";
 import {WMSTileSetLayer} from "@luciad/ria/view/tileset/WMSTileSetLayer.js";
 import {WMSTileSetModel} from "@luciad/ria/model/tileset/WMSTileSetModel.js";
 import {TileSet3DLayer} from "@luciad/ria/view/tileset/TileSet3DLayer.js";
 import {HSPCTilesModel} from "@luciad/ria/model/tileset/HSPCTilesModel.js";
-import "./LuciadMap.css";
 import {
     color,
     distance,
     fraction,
     mixmap,
-    numberParameter,
+    numberParameter, ParameterExpression,
     pointParameter,
     positionAttribute
 } from "@luciad/ria/util/expression/ExpressionFactory.js";
@@ -20,6 +19,10 @@ import {ScalingMode} from "@luciad/ria/view/style/ScalingMode.js";
 import {Bounds} from "@luciad/ria/shape/Bounds.js";
 import {CoordinateReference} from "@luciad/ria/reference/CoordinateReference.js";
 import {createTransformation} from "@luciad/ria/transformation/TransformationFactory.js";
+import Slider from "rc-slider";
+import 'rc-slider/assets/index.css';
+import "./LuciadMap.css";
+
 
 const COLOR_SPAN_HEIGHT= [
     "#000080", /* Deep Water */
@@ -34,10 +37,21 @@ const COLOR_SPAN_HEIGHT= [
 
 const TargetHSPCLayerID = "TARGET-HSPC-LAYER";
 
+interface RangeWithExpressions {
+    min: number;
+    max: number;
+    minParameter: ParameterExpression<number>;
+    maxParameter: ParameterExpression<number>;
+}
+
 export const LuciadMap: React.FC = () => {
     const divElement = useRef(null as null | HTMLDivElement);
     const nativeMap = useRef(null as null | WebGLMap);
     const pointCloudStyle = useRef(null as null |  PointCloudStyle);
+    const [rangeWithExpressions, setRangeWithExpressions] = useState(null as null |  RangeWithExpressions);
+    const [minValue, setMinValue ] = useState(0);
+    const [maxValue, setMaxValue ] = useState(0);
+
 
     useEffect(()=>{
         // Initialize Map
@@ -45,9 +59,12 @@ export const LuciadMap: React.FC = () => {
             nativeMap.current = new WebGLMap(divElement.current, {reference: getReference("EPSG:4978")});
             LoadWMS(nativeMap.current);
             LoadHSPCLayer(nativeMap.current).then(hspcLayer=>{
-                const range  = calculateDistanceToEarthCenter(hspcLayer.bounds);
                 // Create a style for this layer taking into account the min/max from the bounds
-                pointCloudStyle.current = createPointStyle(range);
+                const result = createPointStyle(hspcLayer.bounds);
+                pointCloudStyle.current = result.pointCloudStyle;
+                setRangeWithExpressions(result.range);
+                setMinValue(result.range.minParameter.value);
+                setMaxValue(result.range.maxParameter.value);
                 hspcLayer.pointCloudStyle = pointCloudStyle.current;
             });
         }
@@ -76,6 +93,34 @@ export const LuciadMap: React.FC = () => {
         <div ref={divElement} className="map"/>
         <div className="button-bar">
             <button onClick={toggleStyle}>Toggle Mode</button>
+        </div>
+        <div className="vertical-slider">
+            { rangeWithExpressions &&
+                <Slider range={true}  vertical={true} allowCross={false} min={rangeWithExpressions.min} max={rangeWithExpressions.max}
+                        value={[minValue, maxValue]}
+                        // handleRender={(node, handleProps) => {
+                        //     return (
+                        //         <Tooltip
+                        //             overlayInnerStyle={{ minHeight: "auto" }}
+                        //             overlay={"Height: " + (handleProps.value - rangeWithExpressions.min + rangeWithExpressions.seaLevel.min)}
+                        //             placement="bottom"
+                        //         >
+                        //             {node}
+                        //         </Tooltip>
+                        //     );
+                        // }}
+                        marks={{
+                            [rangeWithExpressions.min]: <div className="mark123">{`${Math.round(-(rangeWithExpressions.max-rangeWithExpressions.min)/2)}`}</div>,
+                            [rangeWithExpressions.max]: <div className="mark123">{`${Math.round(rangeWithExpressions.max-rangeWithExpressions.min-(rangeWithExpressions.max-rangeWithExpressions.min)/2)}`}</div>,
+                        }}
+                        onChange={(value: any)=>{
+                            setMinValue(value[0]);
+                            rangeWithExpressions.minParameter.value = value[0];
+                            setMaxValue(value[1]);
+                            rangeWithExpressions.maxParameter.value = value[1];
+                        }
+                }/>
+            }
         </div>
     </div>)
 }
@@ -123,7 +168,13 @@ function LoadHSPCLayer(map: WebGLMap) {
 
 }
 
-function createPointStyle(range: { min: number; max: number }): PointCloudStyle  {
+function createPointStyle(bounds: Bounds): {
+    pointCloudStyle: PointCloudStyle;
+    range: RangeWithExpressions;
+}  {
+    const range  = calculateFangeDistanceToEarthCenter(bounds);
+    // const rangeSeaLevel = calculateRangeMeterFromSeaLevel(bounds);
+
     const averageHeight = range.min + (range.max-range.min) / 2;
 
     const min = Math.round(averageHeight - 20);
@@ -142,18 +193,26 @@ function createPointStyle(range: { min: number; max: number }): PointCloudStyle 
     });
 
     return {
-        gapFill: 3,
-        pointSize:{
-            mode: ScalingMode.ADAPTIVE_WORLD_SIZE,
-            minimumPixelSize: 2,
-            worldScale: 1
+        pointCloudStyle: {
+            gapFill: 3,
+            pointSize: {
+                mode: ScalingMode.ADAPTIVE_WORLD_SIZE,
+                minimumPixelSize: 2,
+                worldScale: 1
+            },
+            colorExpression: mixmap(heightFraction, colorMix)
         },
-        colorExpression: mixmap(heightFraction, colorMix)
+        range: {
+            min: range.min,
+            max: range.max,
+            minParameter,
+            maxParameter
+        }
     }
 }
 
 
-function calculateDistanceToEarthCenter(boundsIn: Bounds) {
+function calculateFangeDistanceToEarthCenter(boundsIn: Bounds) {
     const bounds = reprojectBounds(boundsIn, getReference("EPSG:4978")) as Bounds;
     const a =  Math.sqrt(bounds.x * bounds.x + bounds.y * bounds.y + bounds.z * bounds.z);
     const b = Math.sqrt((bounds.x+bounds.width) * (bounds.x+bounds.width) + (bounds.y+bounds.height) * (bounds.y+bounds.height) + (bounds.z+bounds.depth) * (bounds.z+bounds.depth));
@@ -162,6 +221,16 @@ function calculateDistanceToEarthCenter(boundsIn: Bounds) {
         max: Math.max(a,b)
     };
 }
+
+// function calculateRangeMeterFromSeaLevel(boundsIn: Bounds) {
+//     const bounds =  reprojectBounds(boundsIn) as Bounds;
+//     const a = bounds.z;
+//     const b = bounds.z+bounds.depth;
+//     return {
+//         min: Math.min(a,b),
+//         max: Math.max(a,b)
+//     };
+// }
 
 function reprojectBounds(shape: Bounds, targetReference?:  CoordinateReference) {
     // When no targetProjection Specified then default to CRS:84 (EPSG:4326);
@@ -178,3 +247,5 @@ function reprojectBounds(shape: Bounds, targetReference?:  CoordinateReference) 
         }
     }
 }
+
+
